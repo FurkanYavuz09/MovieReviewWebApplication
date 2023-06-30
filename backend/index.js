8b1f0e0230eda041cb1413197a078438e5ee2aac
FurkanYavuz09 <furkanyavuz0926@gmail.com>
2023-06-30 15:09:10 +0300

    dsadddd

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,6 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
-// Example usage:
-const newMovie = new Movie({
-  title: 'Fight Club',
-  description: 'An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.',
-  releaseDate: new Date('1999-9-10'),
-  averageRating: 4.4,
-});
-
-newMovie.save()
-  .then((savedMovie) => {
-    console.log('Movie saved:', savedMovie);
-  })
-  .catch((error) => {
-    console.error('Error saving movie:', error);
-  });
 
 
   
